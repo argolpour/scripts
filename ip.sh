2309aa1a590e5151bc2465577d8650c2cd98d049
alireza <argolpour@gmail.com>
2023-01-29 16:58:43 +0330

    ip.sh has been changed

--- ip.sh
+++ ip.sh
@@ -8,6 +8,6 @@ then
 	AVG=`ping -c 1 $ip | tail -1 | awk -F "/" '{ print "AVG:" $5 }'`
 	echo "$ip is available with $AVG ms"
 else
-	echo "$ip is not available"
+	echo "$ip is not available right now"
 fi
 
